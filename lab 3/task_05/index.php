a6fd678835040758ad42926a47d6fdfa32718056
Andrii Soroka <vt231_sao@student.ztu.edu.ua>
2025-04-23 11:04:59 +0300

    можливість підписки не різні івенти lab 4(task 3)

--- lab 3/task_05/index.php
+++ lab 3/task_05/index.php
@@ -2,16 +2,32 @@
 
 require_once 'LightTextNode.php';
 require_once 'LightElementNode.php';
+
 $ul = new LightElementNode('ul', 'block');
 $ul->addClass('my-list');
 
 for ($i = 1; $i <= 3; $i++) {
     $li = new LightElementNode('li', 'block');
     $li->addChild(new LightTextNode("Item $i"));
+
+    $li->addEventListener('click', function ($element) use ($i) {
+        echo "Clicked on item $i\n";
+    });
+
     $ul->addChild($li);
 }
 
-echo "Outer HTML:\n";
+echo "Triggering click on 2nd item \n";
+$ul->triggerEvent('click');
+$ul->getInnerHTML();
+
+echo "Triggering click on LI[2]\n";
+$ulChildren = (new ReflectionClass($ul))->getProperty('children');
+$ulChildren->setAccessible(true);
+$secondLi = $ulChildren->getValue($ul)[1];
+$secondLi->triggerEvent('click');
+
+echo "\nOuter HTML:\n";
 echo $ul->getOuterHTML() . "\n";
 
 echo "\nInner HTML:\n";
